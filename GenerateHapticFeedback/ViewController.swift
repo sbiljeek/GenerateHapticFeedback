7f894170bf2477998a7dd33722122fc202d661c0
sbiljeek <sbiljeekdev@gmail.com>
2022-11-10 13:18:28 +0300

    initial commit

--- GenerateHapticFeedback/ViewController.swift
+++ GenerateHapticFeedback/ViewController.swift
@@ -80,3 +80,45 @@ class ViewController: UIViewController {
     }
 }
 
+enum HapticFeedbackTypes {
+    case Notification
+    case Impact
+    case Selection
+}
+
+fileprivate func generateHapticFeedback(hapticFeedbackType: HapticFeedbackTypes, notificationFeedbackType: UINotificationFeedbackGenerator.FeedbackType? = nil, impactFeedbackType: UIImpactFeedbackGenerator.FeedbackStyle? = nil) {
+    switch hapticFeedbackType {
+    case .Notification:
+        let generator = UINotificationFeedbackGenerator()
+        switch notificationFeedbackType {
+        case .error:
+            generator.notificationOccurred(.error)
+        case .success:
+            generator.notificationOccurred(.success)
+        case .warning:
+            generator.notificationOccurred(.warning)
+        default:
+            generator.notificationOccurred(.success)
+        }
+        
+    case .Impact:
+        switch impactFeedbackType {
+        case .light:
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.impactOccurred()
+        case .medium:
+            let generator = UIImpactFeedbackGenerator(style: .medium)
+            generator.impactOccurred()
+        case .heavy:
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.impactOccurred()
+        default:
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.impactOccurred()
+        }
+        
+    case .Selection:
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
+}
